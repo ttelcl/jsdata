6cc2159976964fdde4e2e5e47c2843b67363a3b6
ttelcl <9429819+ttelcl@users.noreply.github.com>
2023-11-04 17:14:49 +0200

    Now implemented output writing

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 import {
-  parseArguments,
-  loadJson,
   trx,
-  projectToModel
+  runTransformApplication,
 } from "./jstrx/js-trx.js";
 
-const args = [...process.argv]
-
-const filePairs = parseArguments(args)
-
-const model = {
-  character: {
-    name: "",
-    id: 0,
-    realm: {
+const modelLibrary = {
+  default: {
+    character: {
       name: "",
-      id: trx.number,
-      slug: trx.string
+      id: 0,
+      realm: {
+        name: "",
+        id: trx.number,
+        slug: trx.string
+      },
     },
-  },
+    equipped_items: [{
+      item: { id: 0 },
+      slot: { name: "" },
+      name: "",
+    }]
+  }
 }
 
-for (const { inputFile, outputFile } of filePairs) {
-  console.log(` "${inputFile}" -> "${outputFile}"`)
-  const data = loadJson(inputFile)
-  const projected = projectToModel(data, model)
-  console.log(JSON.stringify(projected, null, 2))
-}
+runTransformApplication(modelLibrary, [...process.argv])
--- jstrx/js-trx.js
+++ jstrx/js-trx.js
@@ -1,10 +1,11 @@
-import {readFileSync} from 'fs';
-
-/**
- * @typedef {Object} DataFilePair
- * @property {string} inputFile
- * @property {string} outputFile
- */
+import { 
+  readFileSync,
+  writeFileSync,
+  copyFileSync,
+  rmSync,
+  renameSync,
+  existsSync,
+} from 'node:fs';
 
 /**
  * Function that tries to match the actual data to the given model,
@@ -13,55 +14,20 @@ import {readFileSync} from 'fs';
  */
 
 /**
- * Parse arguments. Currently the only supported arguments are of
- * the shapes "-f input.json output.json" and "-f input.json"
- * @param {string[]} args The arguments to parse
- * @returns {DataFilePair[]}
+ * @typedef {Object} Transformation
+ * @property {string} modelName
+ * @property {string} inputFile
+ * @property {string} outputFile
  */
-export function parseArguments(args) {
-  const results = []
-  while(args.length > 0) {
-    const arg = args.shift()
-    if (arg === "-f") {
-      if (args.length > 0) {
-        const inputFile = args.shift()
-        const extensionIndex = inputFile.lastIndexOf(".")
-        if (extensionIndex < 0) {
-          throw new Error(`Expecting file name to have an extension`)
-        }
-        if (args.length > 0 && !args[0].startsWith("-")) {
-          const outputFile = args.shift()
-          results.push({inputFile, outputFile});
-        } else {
-          const extension = inputFile.slice(extensionIndex)
-          const prefix = inputFile.slice(0, extensionIndex)
-          const outputFile = prefix + ".out" + extension
-          results.push({inputFile, outputFile});
-        }
-      } else {
-        throw new Error(`Expecting a file name after "-f"`)
-      }
-    } else if (arg.endsWith("node.exe")) {
-      const script = args.shift()
-      // ignore
-    } else {
-      throw new Error(`Unexpected argument "${args[0]}"; expecting "-f"`)
-    }
-  }
-  return results
-}
 
 /**
- * Load an existing JSON file
- * @param {string} filename 
- * @returns {any}
+ * Extended version of typeof(), furthe distinguishing "object" into
+ * "null", "array" and "object"
+ * @param {any} value 
+ * @returns {string}
  */
-export function loadJson(filename) {
-  return JSON.parse(readFileSync(filename, "utf8"))
-}
-
-function typeofEx(value) {
-  const t0 = typeof(value)
+export function typeofEx(value) {
+  const t0 = typeof (value)
   switch (t0) {
     case "object":
       return value === null ? "null" : Array.isArray(value) ? "array" : "object"
@@ -71,10 +37,10 @@ function typeofEx(value) {
 }
 
 function projectObject(data, model) {
-  if (data === null || typeof(data) !== "object" || Array.isArray(data)) {
+  if (data === null || typeof (data) !== "object" || Array.isArray(data)) {
     return undefined;
   }
-  if (model === null || typeof(model) !== "object" || Array.isArray(model)) {
+  if (model === null || typeof (model) !== "object" || Array.isArray(model)) {
     throw new Error("Expecting a model that is an object")
   }
   const result = {}
@@ -90,8 +56,35 @@ function projectObject(data, model) {
   return result
 }
 
-function projectArray(data, model) {
-  throw new Error("NYI: projectArray")
+/**
+ * Project an input array to an array of elements matching one of the models
+ * @param {any[]} data The data array to project
+ * @param {any[]} model The array of models to probe for mapping
+ * @param {boolean} nullIfNotMatching If true, non-matching data elements are replaced by
+ * null instead of being skipped altogether
+ * @returns {any[] | undefined}
+ */
+function projectArray(data, model, nullIfNotMatching) {
+  if (data === null || typeof (data) !== "object" || !Array.isArray(data)) {
+    return undefined;
+  }
+  if (model === null || typeof (model) !== "object" || !Array.isArray(model)) {
+    throw new Error("Expecting a model that is an array")
+  }
+  const result = []
+  for (const dataValue of data) {
+    let projected = undefined
+    for (const modelValue of model) {
+      projected = projectAny(dataValue, modelValue)
+      if (projected !== undefined) { break }
+    }
+    if (projected !== undefined) {
+      result.push(projected)
+    } else if (nullIfNotMatching) {
+      result.push(null)
+    }
+  }
+  return result
 }
 
 /**
@@ -139,42 +132,56 @@ function projectAny(data, model) {
   return matcher(data, model);
 }
 
-export function projectToModel(data, model) {
+/**
+ * Project a data object to the named model selected from the given
+ * model library
+ * @param {any} data The data to project
+ * @param {Object.<string,any>} modelLibray A map from model names to models
+ * @param {string | undefined} modelName The name of the model (undefined is interpreted as "default")
+ * @returns {any} The projected data
+ */
+export function projectToModel(data, modelLibrary, modelName) {
+  modelName ??= "default"
+  const model = modelLibrary[modelName]
+  if (model === undefined) {
+    const modelNames = Object.keys(modelLibrary).join(", ")
+    throw new Error(`Unknown model "${modelName}". Known model names are: ${modelNames}`)
+  }
   return projectAny(data, model);
 }
 
 export const trx = {
   string: function (value, model) {
-    if (typeof(value) === "string") {
+    if (typeof (value) === "string") {
       return value;
     }
     return undefined;
   },
 
   number: function (value, model) {
-    if (typeof(value) === "number") {
+    if (typeof (value) === "number") {
       return value;
     }
     return undefined;
   },
 
   boolean: function (value, model) {
-    if (typeof(value) === "boolean") {
+    if (typeof (value) === "boolean") {
       return value;
     }
     return undefined;
   },
 
   object: function (value, model) {
-    if (typeof(value) === "object" && !Array.isArray(value)) {
+    if (typeof (value) === "object" && !Array.isArray(value)) {
       return projectObject(value, model);
     }
     return undefined;
   },
-  
+
   array: function (value, model) {
-    if (typeof(value) === "object" && Array.isArray(value)) {
-      return projectArray(value, model)
+    if (typeof (value) === "object" && Array.isArray(value)) {
+      return projectArray(value, model, false)
     }
     return undefined;
   },
@@ -185,4 +192,108 @@ export const trx = {
 
 }
 
+// ------------------------------------------------------------------------
 
+/**
+ * Parse arguments. Currently the only supported arguments are of
+ * the shapes "-f input.json output.json" and "-f input.json"
+ * @param {string[]} args The arguments to parse
+ * @returns {Transformation[]}
+ */
+export function parseArguments(args) {
+  args = [...args] // clone, se we do not modify the argument itself
+  const results = []
+  let modelName = "default"
+  while (args.length > 0) {
+    const arg = args.shift()
+    if (arg === "-m") {
+      modelName = args.shift()
+    } else if (arg === "-f") {
+      if (args.length > 0) {
+        const inputFile = args.shift()
+        const extensionIndex = inputFile.lastIndexOf(".")
+        if (extensionIndex < 0) {
+          throw new Error(`Expecting file name to have an extension`)
+        }
+        if (args.length > 0 && (!args[0].startsWith("-") || args[0] === "-")) {
+          const outputFile = args.shift()
+          results.push({ modelName, inputFile, outputFile });
+        } else {
+          const extension = inputFile.slice(extensionIndex)
+          const prefix = inputFile.slice(0, extensionIndex)
+          const outputFile = prefix + "." + modelName + ".out" + extension
+          results.push({ modelName, inputFile, outputFile });
+        }
+      } else {
+        throw new Error(`Expecting a file name after "-f"`)
+      }
+    } else if (arg.endsWith("node.exe")) {
+      const script = args.shift()
+      // ignore
+    } else {
+      throw new Error(`Unexpected argument "${arg}"; expecting "-f" or "-m"`)
+    }
+  }
+  return results
+}
+
+/**
+ * Load an existing JSON file
+ * @param {string} filename 
+ * @returns {any}
+ */
+export function loadJson(filename) {
+  return JSON.parse(readFileSync(filename, "utf8"))
+}
+
+/**
+ * Save data to a JSON file. If the target file exists a backup
+ * of the existing content is made first
+ * @param {string} filename The name of the file to save
+ * @param {string | any} data The data to save, either as a pre-formatted JSON string,
+ * or as some item to be converted to JSON
+ */
+export function saveJson(filename, data) {
+  if (existsSync(filename)) {
+    const bakName = filename + ".bak"
+    if (existsSync(bakName)) {
+      rmSync(bakName)
+    }
+    renameSync(filename, bakName)
+  }
+  if (typeof(data) !== "string") {
+    data = JSON.stringify(data, null, 2)
+  }
+  writeFileSync(filename, data) // UTF8 is default
+}
+
+/**
+ * 
+ * @param {any} modelLibrary The library that maps model names to models.
+ * At the minimum this should contain a model named "default"
+ * @param {string[] | undefined} args The application arguments providing
+ * input and output files. If null, process.argv is used
+ */
+export function runTransformApplication(modelLibrary, args) {
+  args ??= [...process.argv]
+  const transformations = parseArguments(args)
+  if (transformations.length == 0) {
+    console.warn("No inputs provided")
+    console.log("Usage:")
+    console.log("  node <?>.js {[-m <model>] {-f <input.json> [<output.json>]}}")
+    const modelNames = Object.keys(modelLibrary).join(", ")
+    console.log(`Known model names are: ${modelNames}`)
+  } else {
+    for (const { modelName, inputFile, outputFile } of transformations) {
+      console.log(` "${inputFile}" -> "${outputFile}" (model "${modelName}")`)
+      const data = loadJson(inputFile)
+      const projected = projectToModel(data, modelLibrary, modelName)
+      const json = JSON.stringify(projected, null, 2)
+      if (outputFile === "-") {
+        console.log(json)
+      } else {
+        saveJson(outputFile, json)
+      }
+    }
+  }
+}
